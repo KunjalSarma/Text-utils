d0d86e22d98e395e1d820e638a7c22667480b363
Kunjal Sarma <kunjalsarma.10@gmail.com>
2023-02-27 00:28:10 +0530

    Updates

---
+++ precache-manifest.5abac268a13ec9322200ee7ed954a98d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a353691f0b1ffa732183",
+    "url": "/Text-utils/static/css/main.87be0c27.chunk.css"
+  },
+  {
+    "revision": "a353691f0b1ffa732183",
+    "url": "/Text-utils/static/js/main.a353691f.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/Text-utils/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Text-utils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "4e645afcc843ccafa720",
+    "url": "/Text-utils/static/js/runtime~main.4e645afc.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Text-utils/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "19076f670b3370f7f6a0ca6284c9aec2",
+    "url": "/Text-utils/index.html"
+  }
+];
